db31e03b2da51674d676a053c217ace5e33a5e64
Leo Sun <shinji_97@hotmail.com>
2012-03-03 18:25:29 -0500

    friendly forwarding and correct user controll

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+
 
   def new
     @title ='Sign up'
@@ -40,7 +42,11 @@ class UsersController < ApplicationController
   private
 
     def authenticate
-      flash[:notice] = 'Please sign in to access this page'
-      redirect_to signin_path unless signed_in?
+      deny_access unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
